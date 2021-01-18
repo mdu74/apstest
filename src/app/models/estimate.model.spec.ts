9f4a1878b3372ba8b6587e317ff1652bdd2fe098
Mdu <mdu74.c@gmail.com>
2021-01-18 10:42:00 +0200

    added ngxs state management

--- src/app/models/estimate.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Estimate } from './estimate.model';
-
-describe('Estimate', () => {
-  it('should create an instance', () => {
-    expect(new Estimate()).toBeTruthy();
-  });
-});
--- src/app/models/firebase-user.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseUser } from './firebase-user.model';
-
-describe('FirebaseUser', () => {
-  it('should create an instance', () => {
-    expect(new FirebaseUser()).toBeTruthy();
-  });
-});
